5bf7defed8fb587613e429f2f01a6c2baf92c1fd
Ergans33 <104145196+Ergans33@users.noreply.github.com>
2022-05-17 02:33:58 +0700

    Add files via upload

---
+++ gaclex/modules/logo.py
@@ -0,0 +1,63 @@
+import os
+import io
+import requests
+import shutil 
+import random
+import re
+import glob
+import time
+
+from io import BytesIO
+from requests import get
+from telethon.tl.types import InputMessagesFilterPhotos
+
+from gaclex import OWNER_ID
+from gaclex.events import register
+from gaclex import telethn
+from PIL import Image, ImageDraw, ImageFont
+
+
+LOGO_LINKS            = ["https://telegra.ph/file/cfd6da4f2c4f098d69876.jpg"
+                         ]
+
+@register(pattern="^/logo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id != OWNER_ID and not quew:
+  await event.reply('`ɢɪᴠᴇ sᴏᴍᴇ ᴛᴇxᴛ ᴛᴏ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ ʙᴀʙʏ​ !`\n`Example /logo Queen Iraa`')
+  return
+ pesan = await event.reply('**Membuat logo...**')
+ try:
+    text = event.pattern_match.group(1)
+    randc = random.choice(LOGO_LINKS)
+    img = Image.open(io.BytesIO(requests.get(randc).content))
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "black"
+    shadowcolor = "blue"
+    fnt = glob.glob("./geezram/resources/fonts/*")
+    randf = random.choice(fnt)
+    font = ImageFont.truetype(randf, 120)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y = ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
+    fname = "queeniraa.png"
+    img.save(fname, "png")
+    await telethn.send_file(event.chat_id, file=fname, caption = f"━━━━━━━━━━━━━━━━\nʟᴏɢᴏ sᴜᴄᴄᴇssꜰᴜʟʟʏ ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ​ [GEEZ/RAM](https://t.me/geezrambot)\n━━━━━━━━━━━━━━━━")         
+    await pesan.delete()
+    if os.path.exists(fname):
+            os.remove(fname)
+ except Exception as e:
+    await event.reply(f'Error, Report @ramsupportt')
+
+
+__mod_name__ = "Lᴏɢᴏ​"
+
+__help__ = """
+❍ /logo (Text) - Create a logo of your given text with random view.`
+"""
